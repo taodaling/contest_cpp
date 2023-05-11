95d77a4f9dcf9b322cbb52e023f2ad3566849591
dalingtao <taodaling@gmail.com>
2023-05-12 01:13:39 +0800

    more

--- solver.cpp
+++ solver.cpp
@@ -1,87 +1,85 @@
 #pragma once
 #include "common.cpp"
 using namespace dalt;
-#include "modint.cpp"
-#include "range_affine_range_sum.cpp"
-#include "segtree.cpp"
-
-using Mi = ModInt998244353;
+#include "math.cpp"
+#include "trie.cpp"
+const int C = 27;
 void SolveOne(int test_id, IStream &in, OStream &out) {
-  int N, M, Q;
-  in >> N >> M >> Q;
-  struct Limit {
-    int l;
-    int r;
-    int x;
-  };
-  Vec<Limit> Ls(Q);
-  for (int i = 0; i < Q; i++) {
-    in >> Ls[i].l >> Ls[i].r >> Ls[i].x;
+  int N;
+  in >> N;
+  using Tr = trie::Trie<i64, C>;
+  Tr::Register(0);
+  var tree = new Tr();
+  Trace(input);
+  for (int i = 0; i < N; i++) {
+    Str T;
+    i64 P;
+    in >> T >> P;
+    Reverse(All(T));
+    var iter = tree;
+    for (var c : T) {
+      iter = iter->get_or_create(c - 'a');
+    }
+    iter->sum = P;
   }
-  Sort(All(Ls), [&](var &a, var &b) {
-    return a.r < b.r;
-  });
-
-  using SBT = sbt::SelfBalanceTreeRegistry<int, int>;
-  using ST = sbt::SegTree<SBT>;
-  var inf = M;
-  var MinOp = [&](int a, int b) { return Min(a, b); };
-  ST::Register(inf, inf, MinOp, MinOp, MinOp);
-  ST st(N + 1);
-  for (var &op : Ls) {
-    st.update(op.l, op.r, op.x);
-  }
-  var limits = st.to_vec();
-  Debug(limits);
-  TreeMap<int, Vec<int>> group_by_limits;
-  TreeMap<int, Vec<Limit>> group_by_x;
-  for (int i = 1; i <= N; i++) {
-    group_by_limits[limits[i]].push_back(i);
-  }
-  for (var &op : Ls) {
-    group_by_x[op.x].push_back(op);
-  }
-
-  sbt::RangeAffineRangeSum<Mi> rars(N + 1);
-  Mi ans = 1;
-  for (var &pair : group_by_limits) {
-    var ls = group_by_x[pair.first];
-    var &indices = pair.second;
-    var ls_iter = ls.begin();
-    var is_iter = indices.begin();
-    rars.update(0, N, 0, 0);
-    rars.update(0, 0, 0, 1);
-    while (ls_iter != ls.end() || is_iter != indices.end()) {
-      if (is_iter != indices.end() &&
-          (ls_iter == ls.end() || *is_iter <= ls_iter->r)) {
-        var index = *is_iter;
-        ++is_iter;
-        var total = rars.query(0, N);
-        rars.update(index, index, 0, total);
-        Assert(limits[index] == pair.first);
-        rars.update(0, index - 1, limits[index], 0);
-        Debug(index);
-        Debug(rars.to_vec());
-      } else {
-        var &item = *ls_iter;
-        ++ls_iter;
-        rars.update(0, item.l - 1, 0, 0);
-        DebugFmtln("[%d, %d]", item.l, item.r);
-        Debug(rars.to_vec());
+  Vec<Vec<Vec<i64>>> profit(C, Vec<Vec<i64>>(C, Vec<i64>(C)));
+  for (int i = 0; i < C; i++) {
+    for (int j = 0; j < C; j++) {
+      for (int k = 0; k < C; k++) {
+        var iter = tree;
+        for (var c : Vec<int>{k, j, i}) {
+          iter = iter->get_or_create(c);
+          profit[i][j][k] += iter->sum;
+        }
       }
     }
-    var local_contrib = rars.query(0, N);
-    ans *= local_contrib;
-    group_by_x.erase(pair.first);
+  }
 
-    Debug(pair);
-    Debug(local_contrib);
-    Debug(rars.to_vec());
+  i64 inf = 1e18;
+  Vec<Vec<i64>> dists(C, Vec<i64>(C, -inf));
+  Vec<Vec<bool>> inq(C, Vec<bool>(C));
+  Vec<Vec<int>> inq_times(C, Vec<int>(C));
+  dists[C - 1][C - 1] = 0;
+  using A2 = Array<int, 2>;
+  Deque<A2> dq;
+
+  var add = [&](int i, int j) {
+    if (!inq[i][j]) {
+      inq[i][j] = true;
+      inq_times[i][j]++;
+      dq.push_back(A2{i, j});
+    }
+  };
+  add(C - 1, C - 1);
+  while (!dq.empty()) {
+    var head = dq.front();
+    dq.pop_front();
+    inq[head[0]][head[1]] = false;
+    if (inq_times[head[0]][head[1]] > Size(dists)) {
+      out << "Infinity";
+      return;
+    }
+    for (int i = 0; i < C - 1; i++) {
+      if (dists[head[1]][i] <
+          dists[head[0]][head[1]] + profit[head[0]][head[1]][i]) {
+        dists[head[1]][i] =
+            dists[head[0]][head[1]] + profit[head[0]][head[1]][i];
+        add(head[1], i);
+      }
+    }
   }
-  if (Size(group_by_x) > 0) {
-    ans = 0;
+
+  Debug(dists);
+  i64 max = -inf;
+  for (int i = 0; i < C; i++) {
+    for (int j = 0; j < C; j++) {
+      if(i == C - 1 && j == C - 1) {
+        continue;
+      }
+      Chmax(max, dists[i][j]);
+    }
   }
-  out << ans << '\n';
+  out << max << '\n';
 }
 
 void SolveMulti(IStream &in, OStream &out) {
--- stress_external_member.cpp
+++ stress_external_member.cpp
@@ -1,63 +1,8 @@
-#include "math.cpp"
 #include "solver.cpp"
 namespace stress {
-void StressSolve(IStream &in, OStream &out) {
-  Trace(generate correct result);
-  int N, M, Q;
-  in >> N >> M >> Q;
-  struct Item {
-    int l, r, x;
-  };
-  Vec<Item> items;
-  for (int i = 0; i < Q; i++) {
-    int l, r, x;
-    in >> l >> r >> x;
-    l--;
-    r--;
-    items.push_back(Item{.l = l, .r = r, .x = x});
-  }
-  var dfs = [&](var &dfs, Vec<int> &values) -> int {
-    if (Size(values) == N) {
-      for (var &item : items) {
-        int max = 0;
-        for (int i = item.l; i <= item.r; i++) {
-          Chmax(max, values[i]);
-        }
-        if (max != item.x) {
-          return 0;
-        }
-      }
-      return 1;
-    }
-    int ans = 0;
-    for (int i = 0; i <= M; i++) {
-      values.push_back(i);
-      ans += dfs(dfs, values);
-      values.pop_back();
-    }
-    return ans;
-  };
-  Vec<int> values;
-  int ans = dfs(dfs, values);
-  out << ans << '\n';
-}
+void StressSolve(IStream &in, OStream &out) {}
 
-void StressGenerate(int test_id, std::mt19937 &mt, OStream &out) {
-  Trace(generating testcase);
-  int N = random_choice(1, 3, mt);
-  int M = random_choice(1, 3, mt);
-  int Q = random_choice(1, 3, mt);
-  out << N << " " << M << " " << Q << '\n';
-  for (int i = 0; i < Q; i++) {
-    int l = random_choice(1, N, mt);
-    int r = random_choice(1, N, mt);
-    int x = random_choice(1, M, mt);
-    if (l > r) {
-      Swap(l, r);
-    }
-    out << l << " " << r << " " << x << '\n';
-  }
-}
+void StressGenerate(int test_id, std::mt19937 &mt, OStream &out) {}
 
 String StressMatch(IStream &input, IStream &ai, IStream &bi) {
   String x, y;
@@ -77,4 +22,4 @@ String StressMatch(IStream &input, IStream &ai, IStream &bi) {
     }
   }
 }
-}  // namespace stress
+}
